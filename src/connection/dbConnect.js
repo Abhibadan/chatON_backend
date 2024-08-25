cb23af62c6965c9fae90373cf5507e389701bbbd
Abhibadan Ghosh <abhibadanghosh1234@gmail.com>
2024-08-25 12:31:03 +0530

    mongo connection string set

--- src/connection/dbConnect.js
+++ src/connection/dbConnect.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose');
-mongoose.connect("mongodb+srv://connectUser:Ae30xXFENtg91dOL@abhibadanghosh.igcasvl.mongodb.net/chatON?retryWrites=true&w=majority&appName=AbhibadanGhosh")
+mongoose.connect(process.env.MONGO_CONNECTION)
 
 .then(()=>console.log(`DB Connection Success`))
 .catch((err)=>console.log(err));
